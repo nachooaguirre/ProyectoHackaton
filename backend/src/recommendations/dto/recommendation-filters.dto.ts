16a7ccecfc18127cd335654fa138d23f5c035a4f
nachooaguirre <ignacioaguirre861@gmail.com>
2025-09-10 19:38:56 -0300

    dalsda

--- backend/src/recommendations/dto/recommendation-filters.dto.ts
+++ backend/src/recommendations/dto/recommendation-filters.dto.ts
@@ -8,10 +8,7 @@ export class RecommendationFiltersDto {
   minRating?: number; // IMDb o general
   count?: number; // 1, 5, 10, etc.
   region?: string; // Código de país TMDb (AR, US, ES, MX, etc.)
-<<<<<<< HEAD
   platforms?: string[]; // Nombres de plataformas deseadas (ej. Netflix, Amazon Prime Video)
-=======
->>>>>>> 0111ec8a9e3c8467df8a07c3768e8eb147391359
 }
 
 
